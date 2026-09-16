d2e1fcac81abfc651eca94f5d80aac5550e53980
Taejin Park <tango4j@gmail.com>
2026-09-15 17:37:17 -0700

    Fix timestamp formatting and copyright headers
    Signed-off-by: Taejin Park <tango4j@gmail.com>

--- tests/collections/asr/test_extract_salm_enc_ctc_timestamps.py
+++ tests/collections/asr/test_extract_salm_enc_ctc_timestamps.py
@@ -1,15 +1,24 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import importlib.util
 from pathlib import Path
 
 
 _TIMESTAMP_SCRIPT = (
-    Path(__file__).resolve().parents[3]
-    / "scripts"
-    / "asr_timestamps"
-    / "extract_salm_enc_multispeaker_timestamps.py"
+    Path(__file__).resolve().parents[3] / "scripts" / "asr_timestamps" / "extract_salm_enc_multispeaker_timestamps.py"
 )
 _SPEC = importlib.util.spec_from_file_location("pee_transformer_ctc_timestamp_cli", _TIMESTAMP_SCRIPT)
 assert _SPEC is not None and _SPEC.loader is not None
